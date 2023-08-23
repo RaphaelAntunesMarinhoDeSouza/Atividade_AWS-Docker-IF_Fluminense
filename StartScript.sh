be0ffa3f884369acce6f532003dc821ff8928300
RaphaelAntunesMarinhoDeSouza <138892189+RaphaelAntunesMarinhoDeSouza@users.noreply.github.com>
2023-08-23 15:17:46 -0300

    Update StartScript.sh

--- StartScript.sh
+++ StartScript.sh
@@ -22,7 +22,7 @@ runcmd:
 - mv /usr/local/bin/docker-compose /bin/docker-compose
 - yum install -y amazon-efs-utils
 - yum install -y nfs-utils
-- file_system_id_1=fs-0fdf921c93a54abce
+- file_system_id_1=<DNS_NAME_DO_EFS>
 - efs_mount_point_1=/mnt/efs
 - mkdir -p "${efs_mount_point_1}"
 - test -f "/sbin/mount.efs" && printf "\n${file_system_id_1}:/ ${efs_mount_point_1} efs tls,_netdev\n" >> /etc/fstab || printf "\n${file_system_id_1}.efs.us-east-1.amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\n" >> /etc/fstab
@@ -36,19 +36,28 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash
-sudo echo "
-    services:
-      wordpress:
-        image: wordpress:latest
-        volumes:
-          - /mnt/efs/wordpress:/var/www/html
-        ports:
-          - "80:80"
-        environment:
-          WORDPRESS_DB_HOST: database-2.cnqrkzyc9bxk.us-east-1.rds.amazonaws.com
-          WORDPRESS_DB_USER: admin
-          WORDPRESS_DB_PASSWORD: 12345678
-          WORDPRESS_DB_NAME: dbdocker
-" > /mnt/efs/docker-compose.yml
+if [ ! -f "/mnt/efs/docker-compose.yml" ]; then
+  sudo echo "
+      services:
+        wordpress:
+          image: wordpress:latest
+          volumes:
+            - /mnt/efs/wordpress:/var/www/html
+          ports:
+            - "80:80"
+          environment:
+            WORDPRESS_DB_HOST: <RDS End point>
+            WORDPRESS_DB_USER: <RDS Master Username>
+            WORDPRESS_DB_PASSWORD: <Master Password>
+            WORDPRESS_DB_NAME: <RDS name, selected in additional settings>
+  " > /mnt/efs/docker-compose.yml
+fi
+if [ ! -f "/mnt/efs/reboot.sh" ]; then
+  sudo echo "
+      sudo docker-compose -f /mnt/efs/docker-compose.yml up -d
+  " > /mnt/efs/reboot.sh
+  sudo chmod +rwx /mnt/efs/reboot.sh
+  echo "@reboot /mnt/efs/reboot.sh" | crontab -
+fi
 sudo docker-compose -f /mnt/efs/docker-compose.yml up -d
 --//--
